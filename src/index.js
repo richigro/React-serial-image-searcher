ab167c5838354bc903e26385752b16b57eb67a7a
Ricardo Guerrero <richi_gro@hotmail.com>
2019-04-03 21:13:46 -0500

    first test commit of version 1 of app

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function App(props) {
-    return <h1>Hello World!</h1>;
+    return <h1>Version 1 of app</h1>;
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
