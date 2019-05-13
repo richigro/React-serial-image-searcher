7f67e8854873840823c3951a29a1f490efe37f4c
Ricardo Guerrero <richi_gro@hotmail.com>
2019-05-12 22:24:20 -0500

    added new folder for new components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-
-function App(props) {
-    return <h1>Version 1 of app</h1>;
-}
+import App from './app';
 
 ReactDOM.render(<App />, document.getElementById('root'));
