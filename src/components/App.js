1a4ccea0694d8a24ba6f639c9a121bdd6a0b11ff
Ricardo Guerrero <richi_gro@hotmail.com>
2019-05-12 23:03:33 -0500

    created an styled search bar ready to be used awaiting to connect to event listeners not yet created

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import SearchBar from './SearchBar';
 
 export default function App() {
     return(
-        <div>
+        <div className="ui container" style={{marginTop: '20px'}}>
             <SearchBar />
         </div>
     );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,9 +3,12 @@ import React from 'react';
 export default class SearchBar extends React.Component {
     render(){
         return(
-            <div>
-                <form>
-                    <input type="text" placeholder="Search any image"/>
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input type="text" placeholder="Search any image"/>
+                    </div>
                 </form>
             </div>
         );
