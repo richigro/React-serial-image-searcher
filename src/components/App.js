82df1de6940dee74fd03328735125fd5691a0742
Ricardo Guerrero <richi_gro@hotmail.com>
2019-06-25 18:50:56 -0500

    added new search functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import SearchBar from './SearchBar';
 import axios from 'axios';
+import SearchBar from './SearchBar';
 
-class App extends React.Component {
-    onSearchSubmit(term) {
-        console.log(term);
+
+export default class App extends React.Component {
+    state = { images: []};
+    
+    onSearchSubmit = async (term) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term},    
+            headers: {
+                    Authorization: 'Client-ID aa10ef6dff3a9a5ed8f40104b040b2aae8d47c2cff635e80cc61e3fd7d9f2a90'
+            }
+        });
+        this.setState({ images: response.data.results});
     }
 
 
@@ -12,9 +21,8 @@ class App extends React.Component {
         return(
             <div className="ui container" style={{marginTop: '20px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
+                Found: {this.state.images.length} images.
             </div>
         );
     }
 }
-
-export default App;
